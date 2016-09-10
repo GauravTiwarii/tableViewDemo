7aad9775a28bc5ac9115f5a39e7406b2a81c1817
Love <Love@love.local>
2016-09-10 08:55:45 +0530

    Initial Commit

---
+++ tableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewDemo
+//
+//  Created by Love on 9/10/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
